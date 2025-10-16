a4af8b3246a415de2936f86f5b0ea01fd95e4224
mwangiKenya <mwangiwacollins@gmail.com>
2025-10-16 10:59:21 +0300

    Fix changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// ✅ Automatically uses '/' locally and '/rahisi/' when building for GitHub Pages
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/rahisi/' : '/',
+  }
 })
